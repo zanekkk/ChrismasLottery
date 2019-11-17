ba64c986d8738482e9b366697e748a74238eaa83
zanekkk <cekarcekar@gmail.com>
2019-11-17 13:58:33 +0100

    First commit

---
+++ src/main/java/kenazcode/christmaslottery/ChristmaslotteryApplication.java
@@ -0,0 +1,13 @@
+package kenazcode.christmaslottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmaslotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmaslotteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kenazcode/christmaslottery/ChristmaslotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package kenazcode.christmaslottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmaslotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
